68a22c086adcb3193abafe5551b86f1bc29beab1
Jopie Krekel <jkrekel@roc-dev.com>
2020-04-28 13:03:28 +0200

    Added Test class

---
+++ src/h02/Test.java
@@ -0,0 +1,12 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Test extends Applet {
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawString("Test", 50, 50);
+    }
+}
